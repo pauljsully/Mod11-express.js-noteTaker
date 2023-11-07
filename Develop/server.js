bb10be36c7cd8f7e8521233997e4f78ab0761f73
Paul Sullivan <paul8sullivan@gmail.com>
2023-11-07 01:37:20 -0500

    added server.js

---
+++ Develop/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.port || 3001;
+
+const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.splice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server at http://localhost:${PORT}`);
+});
